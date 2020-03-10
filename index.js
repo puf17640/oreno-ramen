22fc656df54ecaa1eb2b6339e7c07fd9c39908b5
Julian Pufler <puf17640@spengergasse.at>
2020-03-10 15:19:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+$(() => {
+	lozad().observe()
+	
+	let slideIndex = 0
+	function slider() {
+		let slides = $(".slide")
+		$(slides[slideIndex++]).fadeOut(1500)
+		if(slideIndex >= slides.length){
+			slides.fadeIn(1500)
+			slideIndex = 0
+		}
+	}
+	setInterval(slider, 5000)
+})
